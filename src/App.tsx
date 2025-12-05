1bae79e8a1bf9809a2e97502acc01a457e79ca95
Petya-G <vegvaripeter21@gmail.com>
2025-12-05 22:53:07 +0100

    working calculator

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,35 @@ import { useState } from "react";
 import type { Dispatch, SetStateAction } from "react";
 
 import { createContext, useContext } from "react";
+import { evaluate } from "mathjs";
 
-const InputContext = createContext<
+const AppContext = createContext<
   | {
       input: string;
       setInput: Dispatch<SetStateAction<string>>;
+      result: string;
+      setResult: Dispatch<SetStateAction<string>>;
     }
   | undefined
 >(undefined);
 
 function CalcButton({ value }: { value: string }) {
-  const { input, setInput } = useContext(InputContext)!;
+  const { setInput } = useContext(AppContext)!;
   return (
     <Col>
       <Button onClick={() => setInput((input) => input + value)}>
         {value}
+      </Button>
+    </Col>
+  );
+}
+
+function EqualsButton() {
+  const { input, setResult } = useContext(AppContext)!;
+  return (
+    <Col>
+      <Button onClick={() => setResult(() => evaluate(input))}>
+        =
       </Button>
     </Col>
   );
@@ -32,7 +46,7 @@ function App() {
   const [result, setResult] = useState("");
 
   return (
-    <InputContext value={{ input, setInput }}>
+    <AppContext value={{ input, setInput, result, setResult }}>
       <Container>
         <Container>
           <Row>{result}</Row>
@@ -58,32 +72,26 @@ function App() {
             <CalcButton value="+" />
           </Row>
           <Row>
-            <CalcButton value="7" />
-            <CalcButton value="8" />
-            <CalcButton value="9" />
-            <CalcButton value="+" />
-          </Row>
-          <Row>
             <CalcButton value="4" />
             <CalcButton value="5" />
             <CalcButton value="6" />
-            <CalcButton value="=" />
+            <CalcButton value="3" />
           </Row>
           <Row>
             <CalcButton value="1" />
             <CalcButton value="2" />
             <CalcButton value="3" />
-            <CalcButton value="=" />
+            <EqualsButton/>
           </Row>
           <Row>
             <CalcButton value="0" />
             <CalcButton value="0" />
             <CalcButton value="." />
-            <CalcButton value="=" />
+            <EqualsButton/>
           </Row>
         </Container>
       </Container>
-    </InputContext>
+    </AppContext>
   );
 }
 
