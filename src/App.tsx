4a32a8cb2ece50d9d27e5c53c136637a4d6fe66c
Petya-G <vegvaripeter21@gmail.com>
2025-12-05 23:32:25 +0100

    added columns

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,28 @@ const AppContext = createContext<
   | undefined
 >(undefined);
 
+function useCalculate() {
+  const { input, setResult } = useContext(AppContext)!;
+  return () => {
+    try {
+      setResult(evaluate(input));
+    } catch (error) {
+      setResult("Syntax error");
+    }
+  };
+}
+
 function CalcButton({ value }: { value: string }) {
   const { setInput } = useContext(AppContext)!;
+  const calculate = useCalculate();
   return (
     <Col>
-      <Button onClick={() => setInput((input) => input + value)}>
+      <Button
+        onClick={() => {
+          setInput((input) => input + value);
+          calculate();
+        }}
+      >
         {value}
       </Button>
     </Col>
@@ -31,12 +48,10 @@ function CalcButton({ value }: { value: string }) {
 }
 
 function EqualsButton() {
-  const { input, setResult } = useContext(AppContext)!;
+  const calculate = useCalculate();
   return (
     <Col>
-      <Button onClick={() => setResult(() => evaluate(input))}>
-        =
-      </Button>
+      <Button onClick={calculate}>=</Button>
     </Col>
   );
 }
@@ -49,7 +64,6 @@ function App() {
     <AppContext value={{ input, setInput, result, setResult }}>
       <Container>
         <Container>
-          <Row>{result}</Row>
           <Row>
             <Form.Control
               type="text"
@@ -57,39 +71,96 @@ function App() {
               onChange={(e) => setInput(e.target.value)}
             />
           </Row>
-        </Container>
-        <Container>
           <Row>
-            <CalcButton value="%" />
-            <CalcButton value="/" />
-            <CalcButton value="*" />
-            <CalcButton value="-" />
-          </Row>
-          <Row>
-            <CalcButton value="7" />
-            <CalcButton value="8" />
-            <CalcButton value="9" />
-            <CalcButton value="+" />
-          </Row>
-          <Row>
-            <CalcButton value="4" />
-            <CalcButton value="5" />
-            <CalcButton value="6" />
-            <CalcButton value="3" />
-          </Row>
-          <Row>
-            <CalcButton value="1" />
-            <CalcButton value="2" />
-            <CalcButton value="3" />
-            <EqualsButton/>
-          </Row>
-          <Row>
-            <CalcButton value="0" />
-            <CalcButton value="0" />
-            <CalcButton value="." />
-            <EqualsButton/>
+            <h1>{result}</h1>
           </Row>
         </Container>
+        <Row>
+          <Col>
+            <Row>
+              <CalcButton value="Hyp" />
+              <CalcButton value="mod" />
+            </Row>
+            <Row>
+              <CalcButton value="sin" />
+              <CalcButton value="1/x" />
+            </Row>
+            <Row>
+              <CalcButton value="cos" />
+              <CalcButton value="x!" />
+            </Row>
+            <Row>
+              <CalcButton value="tan" />
+              <CalcButton value="x^2" />
+            </Row>
+            <Row>
+              <CalcButton value="log" />
+              <CalcButton value="sqrt(x)" />
+            </Row>
+            <Row>
+              <CalcButton value="ln" />
+              <CalcButton value="x^y" />
+            </Row>
+            <Row>
+              <CalcButton value="i" />
+              <CalcButton value="x*10^y" />
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <CalcButton value="%" />
+              <CalcButton value="/" />
+              <CalcButton value="*" />
+              <CalcButton value="-" />
+            </Row>
+            <Row>
+              <CalcButton value="7" />
+              <CalcButton value="8" />
+              <CalcButton value="9" />
+              <CalcButton value="+" />
+            </Row>
+            <Row>
+              <CalcButton value="4" />
+              <CalcButton value="5" />
+              <CalcButton value="6" />
+              <CalcButton value="3" />
+            </Row>
+            <Row>
+              <CalcButton value="1" />
+              <CalcButton value="2" />
+              <CalcButton value="3" />
+              <EqualsButton />
+            </Row>
+            <Row>
+              <CalcButton value="0" />
+              <CalcButton value="0" />
+              <CalcButton value="." />
+              <EqualsButton />
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <CalcButton value="C" />
+              <CalcButton value="<-" />
+            </Row>
+            <Row>
+              <CalcButton value="AC" />
+              <CalcButton value="MS" />
+            </Row>
+            <Row>
+              <CalcButton value="(" />
+              <CalcButton value="M" />
+            </Row>
+            <Row>
+              <CalcButton value=")" />
+              <CalcButton value="MR" />
+            </Row>
+            <Row>
+              <CalcButton value="+/-" />
+              <CalcButton value="M+" />
+            </Row>
+          </Col>
+        </Row>
       </Container>
     </AppContext>
   );
